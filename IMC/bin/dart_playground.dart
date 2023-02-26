5ac780d9b3df78bf56db459c37a499dad7cda3fc
alg81 <alexandreaugustolg@gmail.com>
2023-02-26 15:43:43 -0300

    Calculo do IMC com peso e altura passado dentro da funcao main.

---
+++ IMC/bin/dart_playground.dart
@@ -0,0 +1,14 @@
+// Funcao que calcula o IMC a partir do peso e altura.
+void IMC(double peso, double altura) {
+  double imc;
+  imc = (peso / (altura * altura));
+  print('O IMC é: $imc');
+}
+
+void main(List<String> arguments) {
+  // Passando o peso e altura diretamente no código
+  double peso = 80;
+  double altura = 1.7;
+
+  IMC(peso, altura);
+}
