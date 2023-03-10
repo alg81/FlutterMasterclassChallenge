94dca43b4dc60d78e9fa9eea236b57b0b45176c7
alg81 <alexandreaugustolg@gmail.com>
2023-03-10 15:16:18 -0300

    Cálculo dos dígitos verificadores do CPF e validação do mesmo.

--- CPF/bin/main.dart
+++ CPF/bin/main.dart
@@ -1,7 +1,7 @@
 import 'verifCPF.dart';
 
 void main(List<String> args) {
-  String S_CPF = '03840073413';
+  String S_CPF = '03840432596';
   verifCPF CPF = verifCPF(entradaCPF: S_CPF);
   CPF.testaCPF(S_CPF) ? print('CPF Validado') : print('CPF INVÁLIDO!');
 }
